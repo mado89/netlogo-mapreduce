6b9d3fb36b16fecb452ce4f4e678568f0971868f
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2012-09-08 22:45:43 +0200

    Process Inputfiles

--- src/at/dobiasch/mapreduce/SingleNodeRun.java
+++ src/at/dobiasch/mapreduce/SingleNodeRun.java
@@ -1,12 +1,17 @@
 package at.dobiasch.mapreduce;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Map;
 
 import org.nlogo.api.CompilerException;
 import org.nlogo.api.ExtensionException;
 
 import at.dobiasch.mapreduce.framework.Framework;
 import at.dobiasch.mapreduce.framework.WorkspaceBuffer;
+import at.dobiasch.mapreduce.framework.partition.ICheckAndPartition;
+import at.dobiasch.mapreduce.framework.partition.ParallelPartitioner;
 
 public class SingleNodeRun
 {
@@ -31,9 +36,40 @@ public class SingleNodeRun
 			this.size= fw.getConfiguration().getMappers();
 			wb= new WorkspaceBuffer(size ,world, modelpath);
 			wb.compileComands(fw.getConfiguration().getMapper(), fw.getConfiguration().getReducer());
+			
+			ICheckAndPartition part= new ParallelPartitioner();
+			part.init("/tmp", 100);
+			
+			String indir= fw.getConfiguration().getInputDirectory();
+			File inputdir = new File(indir);
+			
+			FilenameFilter filter = new FilenameFilter() {
+			    public boolean accept(File dir, String name) {
+			        return !name.startsWith(".");
+			    }
+			};
+
+			String[] children = inputdir.list(filter);
+			if (children == null)
+			{
+			    // Either dir does not exist or is not a directory
+			}
+			else
+			{
+			    for (int i=0; i<children.length; i++) {
+			        // Get filename of file or directory
+			    	System.out.println(children[i]);
+			        part.addFile(indir + "/" + children[i]);
+			    }
+			}
+			
+			Map<String,String> ret= part.getChecksums();
+			System.out.println(ret);
 		} catch (CompilerException e1){
 			throw new ExtensionException(e1);
 		} catch (IOException e) {
+			throw new ExtensionException(e);
+		} catch (Exception e) {
 			throw new ExtensionException(e);
 		}
 	}
