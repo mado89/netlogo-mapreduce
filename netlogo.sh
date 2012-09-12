ad4038f0ca8b9995be5adde318cd84aff22fdc7a
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2012-09-12 15:11:31 +0200

    Custom run script, needs adoptation to run on other computers

---
+++ netlogo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd "`dirname "$0"`"             # the copious quoting is for handling paths with spaces
+# -Djava.library.path=./lib     ensure JOGL can find native libraries 
+# -Djava.ext.dirs=              ignore any existing JOGL installation
+# -XX:MaxPermSize=128m          avoid OutOfMemory errors for large models
+# -Xmx1024m                     use up to 1GB RAM (edit to increase)
+# -Dfile.encoding=UTF-8         ensure Unicode characters in model files are compatible cross-platform
+# -jar NetLogo.jar              specify main jar
+# "$@"                          pass along any command line arguments
+CP="$CLASSPATH:/media/data/Uni/DA/netlogo-5.0/NetLogo.jar:/media/data/Uni/DA/mapreduce/mapreduce/framework.jar"
+java -cp $CP -Djava.library.path=./lib -Djava.ext.dirs= -XX:MaxPermSize=128m -Xmx1024m -Dfile.encoding=UTF-8 org.nlogo.app.App "$@"
+
