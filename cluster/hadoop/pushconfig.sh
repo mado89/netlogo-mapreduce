00bc91dda4f2cf2d2b12d3832c76cf1714974581
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2012-09-26 14:32:20 +0200

    for 3 nodes

--- cluster/hadoop/pushconfig.sh
+++ cluster/hadoop/pushconfig.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 scp *-site.xml hadoop-env.sh masters slaves user1@192.168.56.101:/media/hadoop/hadoop/conf/
 scp *-site.xml hadoop-env.sh user1@192.168.56.102:/media/hadoop/hadoop/conf/
+scp *-site.xml hadoop-env.sh user1@192.168.56.103:/media/hadoop/hadoop/conf/
 
--- cluster/hadoop/reformat.sh
+++ cluster/hadoop/reformat.sh
@@ -3,7 +3,9 @@
 USER=user1
 MASTER=192.168.56.101
 NODE0=192.168.56.102
+NODE1=192.168.56.103
 CMDS="rm -r /media/hadoop/dfs/data/*; rm -r /media/hadoop/dfs/name/*; rm -r /media/hadoop/tmp/*"
 ssh $USER@$NODE0 "$CMDS"
+ssh $USER@$NODE1 "$CMDS"
 ssh $USER@$MASTER "$CMDS"
 ssh $USER@$MASTER "/media/hadoop/hadoop/bin/hadoop namenode -format"
