eb8e37a5dabcef0bb51c58c205621d871ab5a6f9
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2012-09-26 20:37:46 +0200

    Fix path to input

--- cluster/hadoop/pushdata.sh
+++ cluster/hadoop/pushdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp ../input1/*.txt user1@192.168.56.101:/tmp/input1
+scp ../../../input1/*.txt user1@192.168.56.101:/tmp/input1
 ssh user1@192.168.56.101 "mkdir -p /tmp/input1; cd /media/hadoop/hadoop;bin/hadoop dfs -copyFromLocal /tmp/input1 /; bin/hadoop dfs -ls /input1"
 
