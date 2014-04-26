376152aba650ee77191d482a8225b263570d4cf3
Martin Dobiasch <martin.dobiasch@dobibrennda.at>
2014-04-26 22:27:56 +0200

    Make Launch Script better

--- netlogo.sh
+++ netlogo.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-cd "`dirname "$0"`"             # the copious quoting is for handling paths with spaces
+
+# Set the path to NetLogo
+NETLOGO="/media/data/netlogo-5.0.5/"
+
+# Path to mapreduce install
+MAPREDUCE="/media/data/netlogo-5.0.5/extensions/"
+
+
 # -Djava.library.path=./lib     ensure JOGL can find native libraries 
 # -Djava.ext.dirs=              ignore any existing JOGL installation
 # -XX:MaxPermSize=128m          avoid OutOfMemory errors for large models
@@ -7,7 +14,9 @@ cd "`dirname "$0"`"             # the copious quoting is for handling paths with
 # -Dfile.encoding=UTF-8         ensure Unicode characters in model files are compatible cross-platform
 # -jar NetLogo.jar              specify main jar
 # "$@"                          pass along any command line arguments
-#CP="$CLASSPATH:/media/data/Uni/DA/netlogo-5.0/NetLogo.jar:/media/data/Uni/DA/mapreduce/mapreduce/mapreduce.jar:/media/data/Uni/DA/mapreduce/mapreduce/client-lib.jar"
-CP="$CLASSPATH:/media/data/netlogo-5.0.5/NetLogo.jar:/media/data/Uni/DA/mapreduce/mapreduce/mapreduce.jar:/media/data/Uni/DA/mapreduce/mapreduce/client-lib.jar"
+
+cd "$NETLOGO"             # the copious quoting is for handling paths with spaces
+
+CP="$CLASSPATH:${NETLOGO}NetLogo.jar:${MAPREDUCE}mapreduce/mapreduce.jar:${MAPREDUCE}mapreduce/client-lib.jar"
 java -cp $CP -Djava.library.path=./lib -Djava.ext.dirs= -XX:MaxPermSize=128m -Xmx1024m -Dfile.encoding=UTF-8 org.nlogo.app.App "$@"
 
